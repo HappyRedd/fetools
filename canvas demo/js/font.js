a8846d32c821c425eccdf1905cf6da84e6e33fab
HappyRedd <2303158157@qq.com>
2017-04-26 18:03:04 +0800

    commit
    add

--- canvas demo/js/font.js
+++ canvas demo/js/font.js
@@ -1,4 +1,32 @@
-function cText(){
-    this.left = 0;
-    this.top = 0;
+function cText(oleft,otop,txt,size){
+    this.left = oleft||0;
+    this.top = otop||0;
+    this.txt = txt||'请输入数据';
+    this.font = size||'"30px Arial";'
 }
+cText.prototype.draw = function(ctx){
+    ctx.save();
+    ctx.font= this.font;
+    ctx.fillText( this.txt, this.font, this.left, this.top);
+};
+//动画兼容
+if(!window.requestAnimationFrame){
+    window.requestAnimationFrame =(window.webkitRequestAnimationFrame||
+    window.mozRequestAnimationFrame||
+    window.oRequestAnimationFrame||
+    window.msRequestAnimationFrame||
+    function(callback){
+        return window.setTimeout(callback,1000/60);
+    });
+
+}
+
+if (!window.cancelAnimationFrame) {
+    window.cancelAnimationFrame = (window.cancelRequestAnimationFrame ||
+    window.webkitCancelAnimationFrame || window.webkitCancelRequestAnimationFrame ||
+    window.mozCancelAnimationFrame || window.mozCancelRequestAnimationFrame ||
+    window.msCancelAnimationFrame || window.msCancelRequestAnimationFrame ||
+    window.oCancelAnimationFrame || window.oCancelRequestAnimationFrame ||
+    window.clearTimeout);
+}
+
