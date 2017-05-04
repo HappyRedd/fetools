09836a41b8fa8bdbb80c7580f907f1a384624dcc
HappyRedd <2303158157@qq.com>
2017-05-04 20:09:34 +0800

    add
    addd

---
+++ canvastext/js/text.js
@@ -0,0 +1,73 @@
+/**
+ * Created by hure on 2017/5/4.
+ */
+var TEXT_CACHE_MAX = 5000;
+
+
+
+var textWidthCache = {};
+var textWidthCacheCounter = 0;
+
+function getTextWidth(text, textFont) {
+    var key = text + ':' + textFont;
+    if (textWidthCache[key]) {
+        return textWidthCache[key];
+    }
+
+    var textLines = (text + '').split('\n');
+    var width = 0;
+
+    for (var i = 0, l = textLines.length; i < l; i++) {
+        // measureText 可以被覆盖以兼容不支持 Canvas 的环境
+        width = Math.max(textContain.measureText(textLines[i], textFont).width, width);
+    }
+
+    if (textWidthCacheCounter > TEXT_CACHE_MAX) {
+        textWidthCacheCounter = 0;
+        textWidthCache = {};
+    }
+    textWidthCacheCounter++;
+    textWidthCache[key] = width;
+
+    return width;
+}
+
+function getTextRect(text, textFont, textAlign, textBaseline) {
+    var textLineLen = ((text || '') + '').split('\n').length;
+
+    var width = getTextWidth(text, textFont);
+    // FIXME 高度计算比较粗暴
+    var lineHeight = getTextWidth('国', textFont);
+    var height = textLineLen * lineHeight;
+
+    var rect = new BoundingRect(0, 0, width, height);
+    // Text has a special line height property
+    rect.lineHeight = lineHeight;
+
+    switch (textBaseline) {
+        case 'bottom':
+        case 'alphabetic':
+            rect.y -= lineHeight;
+            break;
+        case 'middle':
+            rect.y -= lineHeight / 2;
+            break;
+        // case 'hanging':
+        // case 'top':
+    }
+
+    // FIXME Right to left language
+    switch (textAlign) {
+        case 'end':
+        case 'right':
+            rect.x -= rect.width;
+            break;
+        case 'center':
+            rect.x -= rect.width / 2;
+            break;
+        // case 'start':
+        // case 'left':
+    }
+
+    return rect;
+}
---
+++ canvastext/js/utils.js
@@ -0,0 +1,92 @@
+/**
+ * Created by hure on 2017/5/4.
+ */
+/**
+ * Created by hure on 2017/4/28.
+ */
+
+window.utils = {};
+//捕获坐标
+window.utils.captureMouse = function(element){
+    var mouse = {x:0,y:0};
+
+    element.addEventListener('mousemove',function(event){
+        var x,y;
+        if(event.pageX||event.pageY){
+            x = event.pageX;
+            y = event.pageY;
+        }else{
+            x = event.clientX + document.body.scrollLeft +document.documentElement.scrollLeft;
+            y = event.clientY + document.body.scrollTop +document.documentElement.scrollTop;
+        }
+
+        x -= element.offsetLeft;
+        y -= element.offsetTop;
+
+        mouse.x = x;
+        mouse.y = y;
+    },false);
+
+    return mouse;
+}
+
+//获取触摸事件坐标
+window.utils.captureTouch = function (element) {
+    var touch = {x: null, y: null, isPressed: false, event: null},
+        body_scrollLeft = document.body.scrollLeft,
+        element_scrollLeft = document.documentElement.scrollLeft,
+        body_scrollTop = document.body.scrollTop,
+        element_scrollTop = document.documentElement.scrollTop,
+        offsetLeft = element.offsetLeft,
+        offsetTop = element.offsetTop;
+
+    element.addEventListener('touchstart', function (event) {
+        touch.isPressed = true;
+        touch.event = event;
+    }, false);
+
+    element.addEventListener('touchend', function (event) {
+        touch.isPressed = false;
+        touch.x = null;
+        touch.y = null;
+        touch.event = event;
+    }, false);
+
+    element.addEventListener('touchmove', function (event) {
+        var x, y,
+            touch_event = event.touches[0];
+
+        if (touch_event.pageX || touch_event.pageY) {
+            x = touch_event.pageX;
+            y = touch_event.pageY;
+        } else {
+            x = touch_event.clientX + body_scrollLeft + element_scrollLeft;
+            y = touch_event.clientY + body_scrollTop + element_scrollTop;
+        }
+        x -= offsetLeft;
+        y -= offsetTop;
+
+        touch.x = x;
+        touch.y = y;
+        touch.event = event;
+    }, false);
+
+    return touch;
+};
+/****
+ * 绑定点击区域
+ * retrun bloole
+ * */
+window.utils.containsPoint = function(rect, x, y){
+    return !(x<rect.x || x>rect.x + rect.width ||
+    y<rect.y || y>rect.y + rect.height);
+};
+
+//intersects
+
+window.utils.intersects = function(rectA, rectB){
+    return !(rectA.x + rectA.width < rectB.x ||
+    rectB.x + rectB.width < rectA.x ||
+    rectA.y + rectA.height < rectB.y ||
+    rectB.y + rectB.height < rectA.y);
+}
