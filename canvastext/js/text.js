742c21363c4a1d0811a046f06bc87b2256a0be50
HappyRedd <2303158157@qq.com>
2017-05-04 20:28:09 +0800

    add
    aaas

--- canvastext/js/text.js
+++ canvastext/js/text.js
@@ -1,73 +1,72 @@
 /**
  * Created by hure on 2017/5/4.
  */
-var TEXT_CACHE_MAX = 5000;
+ function cText(txt,fillStyle,x,y,size,tx,ty,sx,sy,isPress){
+    this.x = x||0;
+    this.y = y||0;
+    this.txt = txt||'请输入数据';
+    this.fillStyle= fillStyle;
+    this.font =size;
+    this.tx = tx||0;
+    this.ty = ty||0;
+    this.w = 0;
+    this.sw =0;
+    this.h = 0;
+    this.sx = sx;
+    this.sy = sy;
+    this.isPress=isPress;
+}
+cText.prototype.draw = function(ctx){
+    ctx.save();
+    //  ctx.translate(this.tx,this.ty);
+    ctx.font =  this.font;
 
-
-
-var textWidthCache = {};
-var textWidthCacheCounter = 0;
-
-function getTextWidth(text, textFont) {
-    var key = text + ':' + textFont;
-    if (textWidthCache[key]) {
-        return textWidthCache[key];
+    this.w= ctx.measureText('四').width;
+    this.h =  ctx.measureText('四').width;
+    this.sw = ctx.measureText(  this.txt).width;
+    ctx.textAlign="bottom";
+    ctx.fillStyle = this.fillStyle;
+    ctx.fillText(this.txt, this.x,this.y);
+    ctx.restore();
+    drawRoundedRect(ctx, "rgba(255,255,255, 0.4)", "rgba(255,255,255, 0.4)", this.x, this.y- this.h,this.sw + 5, this.h + 7, 0);
+    console.log('w:'+this.sw)
+};
+cText.prototype.getBounds = function(){
+    return {
+        x:  this.x,
+        y:   this.y- this.h,
+        width: this.sw,
+        height: this.h,
+    };
+};
+//rect
+function roundedRect(ctx,cornerX, cornerY, width, height, cornerRadius) {
+    if (width> 0) ctx.moveTo(cornerX + cornerRadius, cornerY);
+    else  ctx.moveTo(cornerX - cornerRadius, cornerY);
+    ctx.arcTo(cornerX+width,cornerY,cornerX + width,cornerY+height,cornerRadius);
+    ctx.arcTo(cornerX+width,cornerY + height,cornerX,cornerY+height,cornerRadius);
+    ctx.arcTo(cornerX,cornerY+height,cornerX,cornerY,cornerRadius);
+    if(width> 0) {
+        ctx.arcTo(cornerX,cornerY,cornerX+cornerRadius,cornerY,cornerRadius);
     }
-
-    var textLines = (text + '').split('\n');
-    var width = 0;
-
-    for (var i = 0, l = textLines.length; i < l; i++) {
-        // measureText 可以被覆盖以兼容不支持 Canvas 的环境
-        width = Math.max(textContain.measureText(textLines[i], textFont).width, width);
+    else{
+        ctx.arcTo(cornerX,cornerY,cornerX-cornerRadius,cornerY,cornerRadius);
     }
-
-    if (textWidthCacheCounter > TEXT_CACHE_MAX) {
-        textWidthCacheCounter = 0;
-        textWidthCache = {};
-    }
-    textWidthCacheCounter++;
-    textWidthCache[key] = width;
-
-    return width;
 }
 
-function getTextRect(text, textFont, textAlign, textBaseline) {
-    var textLineLen = ((text || '') + '').split('\n').length;
+function drawRoundedRect(ctx,strokeStyle,fillStyle,cornerX,cornerY,width,height,cornerRadius) {
+    ctx.beginPath();
+    roundedRect(ctx,cornerX, cornerY, width, height, cornerRadius);
+    ctx.strokeStyle = strokeStyle;
+    ctx.fillStyle = fillStyle;
+    ctx.stroke();
+    ctx.fill();
 
-    var width = getTextWidth(text, textFont);
-    // FIXME 高度计算比较粗暴
-    var lineHeight = getTextWidth('国', textFont);
-    var height = textLineLen * lineHeight;
-
-    var rect = new BoundingRect(0, 0, width, height);
-    // Text has a special line height property
-    rect.lineHeight = lineHeight;
-
-    switch (textBaseline) {
-        case 'bottom':
-        case 'alphabetic':
-            rect.y -= lineHeight;
-            break;
-        case 'middle':
-            rect.y -= lineHeight / 2;
-            break;
-        // case 'hanging':
-        // case 'top':
-    }
-
-    // FIXME Right to left language
-    switch (textAlign) {
-        case 'end':
-        case 'right':
-            rect.x -= rect.width;
-            break;
-        case 'center':
-            rect.x -= rect.width / 2;
-            break;
-        // case 'start':
-        // case 'left':
-    }
-
-    return rect;
+}
+//绘制长方形
+function drawFillRect(cxt,x, y, a, b) {
+    cxt.save();
+    cxt.fillStyle = "rgba(255,255,255, 0.4)";
+    cxt.fillRect(x, y, a, b);
+    cxt.stroke();
 }
